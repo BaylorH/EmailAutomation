d28efdf46289c7d6fd0c5027660837dd106278b0
Baylor Harrison <71840975+BaylorH@users.noreply.github.com>
2025-06-20 10:20:24 -0700

    Update firebase_helpers.py

--- firebase_helpers.py
+++ firebase_helpers.py
@@ -3,7 +3,11 @@ import requests
 FIREBASE_BUCKET = "email-automation-cache.appspot.com"
 
 def download_token(api_key: str, output_file="msal_token_cache.bin", user_id="default_user"):
-    url = f"https://firebasestorage.googleapis.com/v0/b/{FIREBASE_BUCKET}/o/msal_caches%2F{user_id}%2Fmsal_token_cache.bin?alt=media&key={api_key}"
+    url = (
+        f"https://firebasestorage.googleapis.com/v0/b/"
+        f"{FIREBASE_BUCKET}/o/msal_caches%2F{user_id}%2Fmsal_token_cache.bin"
+        f"?alt=media&key={api_key}"
+    )
     r = requests.get(url)
     if r.status_code == 200:
         with open(output_file, "wb") as f:
@@ -18,8 +22,9 @@ def upload_token(api_key: str, input_file="msal_token_cache.bin", user_id="defau
     
     object_path = f"msal_caches/{user_id}/msal_token_cache.bin"
     url = (
-        f"https://firebasestorage.googleapis.com/upload/storage/v1/b/"
-        f"{FIREBASE_BUCKET}/o?uploadType=media&name={object_path}&key={api_key}"
+        f"https://firebasestorage.googleapis.com/v0/b/"
+        f"{FIREBASE_BUCKET}/o/msal_caches%2F{user_id}%2Fmsal_token_cache.bin"
+        f"?uploadType=media&name=msal_caches/{user_id}/msal_token_cache.bin&key={api_key}"
     )
     
     headers = {"Content-Type": "application/octet-stream"}
