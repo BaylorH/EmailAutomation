c95e1ab67a219009e9052933c83bce7331c0d89d
Baylor Harrison <71840975+BaylorH@users.noreply.github.com>
2025-06-24 17:19:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     user_ids = ["6h0p7yYDnSZOd5CAy6qCvs4zw4D2"]
     for uid in user_ids:
         # fetch client emails from Firestore as you already do...
-        recipients = ["bp21harrison#gmail.com"]
+        recipients = ["bp21harrison@gmail.com"]
         send_weekly_email(uid, recipients)
 
 if __name__ == "__main__":
