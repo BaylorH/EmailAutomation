871dddb78f4ae23874f4d16ef46644d863ec7432
Baylor Harrison <71840975+BaylorH@users.noreply.github.com>
2025-06-25 14:02:06 -0700

    Update auth_server.py

--- auth_server.py
+++ auth_server.py
@@ -51,4 +51,5 @@ def start_auth():
         return html  # Still waiting
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
