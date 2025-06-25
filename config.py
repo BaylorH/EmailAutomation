e885f31e57f8cfbfccaae779e6c142acb5475990
Baylor Harrison <71840975+BaylorH@users.noreply.github.com>
2025-06-24 17:06:34 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os, json
 
 # MSAL / Azure AD
 CLIENT_ID     = os.getenv("AZURE_API_APP_ID")
-CLIENT_SECRET = os.getenv("AZURE_API_APP_SECRET")
+CLIENT_SECRET = os.getenv("AZURE_API_CLIENT_SECRET")
 TENANT_ID     = os.getenv("AZURE_TENANT_ID")
 if not TENANT_ID:
     raise RuntimeError("AZURE_TENANT_ID is not set")
