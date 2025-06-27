7f65fb780d39dff86947e5763bdf6ceeaebf56db
BaylorH <bp21harrison@gmail.com>
2025-06-27 13:25:47 -0700

    force refresh for testing

--- scheduler_runner.py
+++ scheduler_runner.py
@@ -66,7 +66,7 @@ for a in accounts:
 
 result = None
 if accounts:
-    result = app.acquire_token_silent(SCOPES, account=accounts[0])
+    result = app.acquire_token_silent(SCOPES, account=accounts[0], force_refresh=True)
     print("🎯 acquire_token_silent() result:", result)
 
 if not result or "access_token" not in result:
